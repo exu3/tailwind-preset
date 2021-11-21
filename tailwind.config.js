0322c21d0a71610d1f959d4cd3441a01e9ba434b
eilla1 <git@ella.cx>
2021-11-20 18:26:30 -0800

    add colors, but fon'ts don't work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,56 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      darker: "#121217",
+      dark: "#17171d",
+      darkless: "#252429",
+      black: "#1f2d3d",
+      steel: "#273444",
+      slate: "#3c4858",
+      muted: "#8492a6",
+      smoke: "#e0e6ed",
+      snow: "#f9fafc",
+      white: "#ffffff",
+      red: "#ec3750",
+      orange: "#ff8c37",
+      yellow: "#f1c40f",
+      green: "#33d6a6",
+      cyan: "#5bc0de",
+      blue: "#338eda",
+      purple: "#a633d6",
+      twitter: "#1da1f2",
+      facebook: "#3b5998",
+      instagram: "#e1306c",
+      text: "#1f2d3d",
+      background: "#ffffff",
+      elevated: "#ffffff",
+      sheet: "#f9fafc",
+      sunken: "#e0e6ed",
+      border: "#e0e6ed",
+      placeholder: "#8492a6",
+      secondary: "#3c4858",
+      primary: "#ec3750",
+      accent: "#338eda",
+    },
+    fontfamily: {
+      sans: [
+        '"Phantom Sans", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif',
+      ],
+      monospace: ['"SF Mono", "Roboto Mono", Menlo, Consolas, monospace'],
+    },
+    extend: {
+      fontfamily: {
+        sans: [
+          '"Phantom Sans", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif',
+        ],
+        monospace: ['"SF Mono", "Roboto Mono", Menlo, Consolas, monospace'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
